55cd897dcff3357f483a5e3c2b4c5d0ae7705eb3
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-07-16 11:44:39 +0300

    Add files via upload

--- make_run.bat
+++ make_run.bat
@@ -3,12 +3,12 @@
 rem Copyright 2026 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2026-05-25
+rem 2026-07-17
 rem https://github.com/Aleksandr3Bocharov/refalabbrainfuck
 
 if not exist bin mkdir bin
-%REFALABBIN%\refalabc src\refalabbrainfuck -a,"-o src\refalabbrainfuck.o" > refalabbrainfuck.log
-%REFALABBIN%\refalabc src\interpretator -a,"-o src\interpretator.o" >> refalabbrainfuck.log
+%REFALABBIN%\refalabc src\refalabbrainfuck -l,"-o src\refalabbrainfuck.o -O2 -Wno-override-module" > refalabbrainfuck.log
+%REFALABBIN%\refalabc src\interpretator -l,"-o src\interpretator.o -O2 -Wno-override-module" >> refalabbrainfuck.log
 clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c src\interpretatorc.c -o src\interpretatorc.o >> refalabbrainfuck.log
-clang -o bin\refalabbrainfuck.exe src\refalabbrainfuck.o src\interpretator.o src\interpretatorc.o %REFALABLIB%\obj\mainrf.o -L%REFALABLIB% -lrefalab -pthread -static >> refalabbrainfuck.log
+clang -o bin\refalabbrainfuck.exe src\refalabbrainfuck.o src\interpretator.o src\interpretatorc.o %REFALABLFLAGS% -static >> refalabbrainfuck.log
 bin\refalabbrainfuck %1
--- make_run_gui.bat
+++ make_run_gui.bat
@@ -3,7 +3,7 @@
 rem Copyright 2026 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2026-05-25
+rem 2026-07-17
 rem https://github.com/Aleksandr3Bocharov/refalabbrainfuck
 
 set RAYLIB=F:\Programming\raylib-5.5_win64_mingw-w64
@@ -11,10 +11,10 @@ set RAYLIBCFLAGS=-Wall -O2 -I%RAYLIB%\include
 set RAYLIBLFLAGS=-L%RAYLIB%\lib -lraylib -lgdi32 -lwinmm
 
 if not exist bin mkdir bin
-%REFALABBIN%\refalabc src\refalabbrainfuck_gui -a,"-o src\refalabbrainfuck_gui.o" > refalabbrainfuck_gui.log
-%REFALABBIN%\refalabc src\interpretator -a,"-o src\interpretator.o" >> refalabbrainfuck_gui.log
+%REFALABBIN%\refalabc src\refalabbrainfuck_gui -l,"-o src\refalabbrainfuck_gui.o -O2 -Wno-override-module" > refalabbrainfuck_gui.log
+%REFALABBIN%\refalabc src\interpretator -l,"-o src\interpretator.o -O2 -Wno-override-module" >> refalabbrainfuck_gui.log
 clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c src\interpretatorc.c -o src\interpretatorc.o >> refalabbrainfuck_gui.log
 clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c src\refalabbrainfuckc_gui.c -o src\refalabbrainfuckc_gui.o >> refalabbrainfuck_gui.log
 clang %RAYLIBCFLAGS% -c src\gui.c -o src\gui.o >> refalabbrainfuck_gui.log
-clang -o bin\refalabbrainfuck_gui.exe src\refalabbrainfuck_gui.o src\refalabbrainfuckc_gui.o src\interpretator.o src\interpretatorc.o %REFALABLIB%\obj\mainrf.o -L%REFALABLIB% -lrefalab src\gui.o -pthread -static %RAYLIBLFLAGS% >> refalabbrainfuck_gui.log
+clang -o bin\refalabbrainfuck_gui.exe src\refalabbrainfuck_gui.o src\refalabbrainfuckc_gui.o src\interpretator.o src\interpretatorc.o %REFALABLFLAGS% src\gui.o -static %RAYLIBLFLAGS% >> refalabbrainfuck_gui.log
 bin\refalabbrainfuck_gui
